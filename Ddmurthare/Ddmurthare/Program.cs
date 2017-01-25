6bea4bc6e395a77f8e65a543fbb522ca4fcc2aa7
Wendel Fabian Chinsamy <wendelfabianchinsamy@gmail.com>
2017-01-25 18:33:56 +0200

    Add files via upload

---
+++ Ddmurthare/Ddmurthare/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+
+namespace Ddmurthare
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var proceed = "y";
+
+            do
+            {
+                Console.Write("Enter proposed chemical name:  ");
+                var chemicalName = Console.ReadLine();
+
+                Console.Write("Enter proposed chemical symbol:  ");
+                var chemicalSymbol = Console.ReadLine();
+
+                var result = VerifyProposal(chemicalName.Trim(), chemicalSymbol.Trim());
+                var status = result ? "Accepted" : "Rejected";
+
+                Console.WriteLine($"Status:  {status}");
+                Console.Write("Would you like to continue?  Type in 'y' and hit enter:  ");
+
+                proceed = Console.ReadLine();
+
+                Console.WriteLine();
+            }
+            while (proceed.ToLower().Equals("y"));
+        }
+
+        private static bool VerifyProposal(string chemicalName, string chemicalSymbol)
+        {
+            // Ensure that name and symbol are valid strings
+            if (string.IsNullOrWhiteSpace(chemicalName) || string.IsNullOrWhiteSpace(chemicalSymbol))
+                return false;
+
+            // Ensure that the symbol length is exactly two
+            if (chemicalSymbol.Length != 2)
+                return false;
+
+            // Ensure that name and symbol contain letters only
+            if (!chemicalName.All(char.IsLetter) || !chemicalSymbol.All(char.IsLetter)) 
+                return false;
+
+            //Ensure that the both letters in the symbol exist in the name
+            if (!chemicalName.ToLower().ToCharArray().Contains(chemicalSymbol.ToLower()[0]) ||
+                !chemicalName.ToLower().ToCharArray().Contains(chemicalSymbol.ToLower()[1]))
+                return false;
+
+            // Ensure that the letters in the symbol appear in the order they appear in the name
+            if (chemicalName.ToLower().IndexOf(chemicalSymbol.ToLower()[0]) >
+                chemicalName.ToLower().LastIndexOf(chemicalSymbol.ToLower()[1]))
+                return false;
+
+            // Ensure that if the symbol has only one distinct letter, that this letter appears more than once in the name
+            if (chemicalSymbol.ToLower().Distinct().Count() == 1 && 
+                chemicalName.ToLower().Count(m => m == chemicalSymbol.ToLower()[0]) < 2)
+                return false;
+
+            // Ensure that only the first letter of the name and symbol are capital
+            if (!IsOnlyFirstLetterCapital(chemicalName) || !IsOnlyFirstLetterCapital(chemicalSymbol))
+                return false;
+
+            return true;
+        }
+
+        private static bool IsOnlyFirstLetterCapital(string value)
+        {
+            // If all letters are upper or lower just return false
+            if (value.All(char.IsUpper) || value.All(char.IsLower))
+                return false;
+
+            for (var i = 0; i < value.Length; i++)
+            {
+                if (char.IsUpper(value[i]))
+                    if (i > 0)
+                        return false;
+            }
+
+            return true;
+        }
+    }
+}
